84c90e912c15de9a3c90cf1f4cdc602d63e96e77
Smet19 <smet1990@gmail.com>
2019-06-17 00:39:02 +0300

    Чисти!

--- ktt/ktt/config.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-import flask_sqlalchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
-from LoginForm import LoginForm
-from RegisterForm import RegisterForm
-from flask_bootstrap import Bootstrap
-
